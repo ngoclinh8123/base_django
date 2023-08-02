4216c6b8d71264cc6a1fb2c2430b93babce0a1a1
ngoclinh8123 <ngoclinh8123@gmail.com>
2023-08-02 17:24:22 +0700

    fix endpoint account

--- core/celery.py
+++ core/celery.py
@@ -1,8 +1,8 @@
 from celery import Celery
 
 app = Celery(
-    "home",
-    broker="amqps://b-af1001fb-b5ff-43fd-80b3-a4afffd06f8f.mq.ap-southeast-2.amazonaws.com:5671",
+    "tasks",
+    broker="amqps://ngoclinh8123:Ngoclinh08012003@b-af1001fb-b5ff-43fd-80b3-a4afffd06f8f.mq.ap-southeast-2.amazonaws.com:5671",
 )
 
 
